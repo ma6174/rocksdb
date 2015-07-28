29b0c4a04df9076bc59e8d74ca26b46bdb48a33b
tuanjie <wangtuanjiezh@gmail.com>
2015-07-28 22:28:08 +0800

    update cgo_flags.go

--- cgo_flags.go
+++ cgo_flags.go
@@ -4,10 +4,8 @@ import (
 	_ "github.com/dckit/c-rocksdb"
 )
 
-// #cgo CXXFLAGS: -std=c++11
 // #cgo CPPFLAGS: -I ../c-rocksdb/internal/include
 // #cgo CPPFLAGS: -I ../../cockroachdb/c-snappy/internal -I ../../cockroachdb/c-lz4/internal/lib
-// #cgo CPPFLAGS: -DROCKSDB_PLATFORM_POSIX -DNDEBUG -DSNAPPY -DLZ4
 // #cgo darwin LDFLAGS: -Wl,-undefined -Wl,dynamic_lookup
 // #cgo !darwin LDFLAGS: -Wl,-unresolved-symbols=ignore-all
 import "C"
